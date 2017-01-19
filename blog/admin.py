3441c87cebdc77f22c946e6100853caedb1c5fd9
jinlixin <444989527@qq.com>
2017-01-19 22:15:37 +0800

    init

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from blog.models import Post
 from blog.models import BlogPostAdmin
 
-admin.site.register(Post, BlogPostAdmin)
+#admin.site.register(Post, BlogPostAdmin)
+admin.site.register(Post)
